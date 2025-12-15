58ebc8c6ced6ac9fcc609a36e2f8d72a0a5edbd0
SeHoon Oh <83574029+sedoll@users.noreply.github.com>
2025-12-15 21:12:13 +0900

    [Silver IV] Title: 카드2, Time: 184 ms, Memory: 28956 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        int n = Integer.parseInt(br.readLine());
+        for(int i=1; i<=n; i++) {
+            deque.add(i);
+        }
+
+        while(true) {
+            if (deque.size() == 1) break;
+            deque.removeFirst(); // 없앤다.
+            if (deque.size() == 1) break;
+            deque.addLast(deque.removeFirst()); // 맨 첫번째 값을 맨 뒤에 넣는다.
+        }
+        System.out.println(deque.pop());
+    }
+}

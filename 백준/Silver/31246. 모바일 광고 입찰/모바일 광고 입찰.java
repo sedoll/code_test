030af28f4a5f6d651d708f7b678a8902111d6a83
SeHoon Oh <83574029+sedoll@users.noreply.github.com>
2026-04-02 21:45:34 +0900

    [Silver V] Title: 모바일 광고 입찰, Time: 524 ms, Memory: 47096 KB -BaekjoonHub

---
+++ 백준/Silver/31246. 모바일 광고 입찰/모바일 광고 입찰.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        List<Integer> list = new ArrayList<>();
+        for (int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list.add(b-a);
+        }
+        Collections.sort(list);
+        int result = list.get(K-1);
+        if (result <= 0) {
+            System.out.println(0);
+        } else {
+            System.out.println(result);
+        }
+    }
+
+
+}
